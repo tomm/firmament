642fac2547e015bb4e82f4fb6e418649ac39081a
Tom Morton <tomm@riseup.net>
2017-09-12 22:06:28 +0100

    GL 3

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate gl;
 use std::f32;
 use std::io::prelude::*;
 use std::fs::File;
+use std::ffi::CString;
 
 mod gl1x;
 
@@ -86,20 +87,130 @@ fn load_catalogue() -> Vec<Star> {
     contents.lines().filter_map(parse_catalogue_line).collect::<Vec<Star>>()
 }
 
-fn main() {
-    let catalogue = load_catalogue();
+mod glazy {
+    use gl;
+    use std;
+    use std::ffi::CString;
 
+    pub struct Shader(u32);
+
+    impl Shader {
+        pub fn id(&self) -> u32 {
+            let Shader(id) = *self;
+            id
+        }
+
+        pub unsafe fn new(vert_src: &str, frag_src: &str) -> Shader {
+            let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+            let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+            gl::ShaderSource(vertex_shader, 1, &CString::new(vert_src).unwrap().as_ptr(), std::ptr::null());
+            gl::ShaderSource(fragment_shader, 1, &CString::new(frag_src).unwrap().as_ptr(), std::ptr::null());
+            gl::CompileShader(vertex_shader);
+            gl::CompileShader(fragment_shader);
+
+            let shader_program = gl::CreateProgram();
+            gl::AttachShader(shader_program, vertex_shader);
+            gl::AttachShader(shader_program, fragment_shader);
+            gl::LinkProgram(shader_program);
+            Shader(shader_program)
+        }
+
+        pub unsafe fn getUniformLocation(&self, name: &str) -> i32 {
+            gl::GetUniformLocation(self.id(), CString::new(name).unwrap().as_ptr())
+        }
+    }
+}
+
+fn main() {
     let context = osvr::Context::new("Rust OSVR example");
     let mut render = osvr::RenderManager::new(&context).unwrap();
     gl1x::init();
     unsafe {
         gl::Disable(gl::DEPTH_TEST);
         gl::Enable(gl::BLEND);
-        gl::Enable(gl1x::POINT_SMOOTH);
+        gl::Enable(gl::PROGRAM_POINT_SIZE);
         gl::BlendFunc(gl::ONE, gl::ONE);
     };
     context.update();
     render.register_buffers();
+
+    let catalogue = load_catalogue();
+
+    let (star_vbo, shader_program, proj_matrix, view_matrix) = {
+
+        let mut star_data: Vec<f32> = Vec::new();
+
+        for s in &catalogue {
+            let (ra, dec) = s.equatorial_position_as_radians();
+
+            star_data.push(-10.0 * ra.sin() * dec.cos());
+            star_data.push( 10.0 * dec.sin());
+            star_data.push(-10.0 * ra.cos() * dec.cos());
+
+            let a = ((6.0 - s.visual_mag)/7.0).max(0.0);
+
+            for color_component in match s.spec_type {
+                // [r, g, b]
+                SpecType::O => [a, a, a],
+                SpecType::B => [a, a, a],
+                SpecType::A => [a, a, a],
+                SpecType::F => [a, a, a],
+                SpecType::G => [a, a, a*0.9],
+                SpecType::K => [a, a*0.9, a*0.75],
+                SpecType::M => [a, a*0.75, a*0.5],
+            }.iter() {
+                star_data.push(*color_component)
+            }
+        }
+
+        unsafe {
+            let mut star_vbo = 0;
+
+            gl::GenBuffers(1, &mut star_vbo);
+            gl::BindBuffer(gl::ARRAY_BUFFER, star_vbo);
+            gl::BufferData(gl::ARRAY_BUFFER, (star_data.len()*4) as isize, std::mem::transmute(&star_data[0]), gl::STATIC_DRAW);
+            gl::BindBuffer(gl::ARRAY_BUFFER, star_vbo);
+            gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 6*4, std::mem::transmute(0u64));
+            gl::EnableVertexAttribArray(0);
+            gl::VertexAttribPointer(1, 3, gl::FLOAT, gl::FALSE, 6*4, std::mem::transmute(12u64));
+            gl::EnableVertexAttribArray(1);
+            gl::BindBuffer(gl::ARRAY_BUFFER, star_vbo);
+
+            let shader = glazy::Shader::new(
+                r#"
+                #version 130 // Specify which version of GLSL we are using.
+
+                uniform mat4 view_matrix, proj_matrix;
+                in vec3 in_Position;
+                in vec3 in_Color;
+                varying vec4 color;
+
+                void main() 
+                {
+                    gl_PointSize = max(1.0, in_Color.r * 3.0);
+                    color = vec4(in_Color.r, in_Color.g, in_Color.b, 1.0);
+                    gl_Position = proj_matrix * view_matrix * vec4(in_Position.x, in_Position.y, in_Position.z, 1.0);
+                }
+            "#, r#"
+                #version 130 // Specify which version of GLSL we are using.
+                varying vec4 color;
+
+                void main() 
+                {
+                    gl_FragColor = color;
+                }
+            "#);
+            gl::BindAttribLocation(shader.id(), 0, CString::new("in_Position").unwrap().as_ptr());
+            gl::BindAttribLocation(shader.id(), 1, CString::new("in_Color").unwrap().as_ptr());
+            let proj_matrix = shader.getUniformLocation("proj_matrix");
+            let view_matrix = shader.getUniformLocation("view_matrix");
+
+            let e = gl::GetError();
+            if e != 0 { panic!("GL ERROR {}", e) };
+            
+            (star_vbo, shader.id(), proj_matrix, view_matrix)
+        }
+    };
 
     loop {
         context.update();
@@ -111,38 +222,25 @@ fn main() {
             let modelview = osvr::glutil::get_modelview(render_info);
 
             unsafe {
-                gl1x::MatrixMode(gl1x::PROJECTION);
-                gl1x::LoadIdentity();
-                gl1x::MultMatrixd(&projection);
-
-                gl1x::MatrixMode(gl1x::MODELVIEW);
-                gl1x::LoadIdentity();
-                gl1x::MultMatrixd(&modelview);
+                let mut _projection: [f32; 16] = std::mem::zeroed();
+                let mut _modelview: [f32; 16] = std::mem::zeroed();
+                for i in 0..16 {
+                    _projection[i] = projection[i] as f32;
+                    _modelview[i] = modelview[i] as f32;
+                }
 
                 gl::ClearColor(0.0, 0.0, 0.0, 1.0);
                 gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
 
-                draw_stars(&catalogue);
+                // draw stars
+                gl::EnableVertexAttribArray(0);
+                gl::EnableVertexAttribArray(1);
+                gl::BindBuffer(gl::ARRAY_BUFFER, star_vbo);
+                gl::UseProgram(shader_program);
+                gl::UniformMatrix4fv(proj_matrix, 1, 0, &_projection[0]);
+                gl::UniformMatrix4fv(view_matrix, 1, 0, &_modelview[0]);
+                gl::DrawArrays(gl1x::POINTS, 0, catalogue.len() as i32);
             }
         });
     }
-}
-
-unsafe fn draw_stars(stars: &Vec<Star>) {
-    gl1x::PointSize(2.0);
-    gl1x::Begin(gl1x::POINTS);
-    
-    for s in stars {
-        let r: f32 = 10.0;
-        let (ra, dec) = s.equatorial_position_as_radians();
-        let brightness = ((6.0 - s.visual_mag)/7.0).min(1.0).max(0.0);
-        let col: [f32; 3] = [brightness, brightness, brightness];
-        gl1x::Color3fv(&col);
-        gl1x::Vertex3f(-r * ra.sin() * dec.cos(),
-                       r * dec.sin(),
-                       -r * ra.cos() * dec.cos());
-
-    }
-
-    gl1x::End();
 }
